e45a93d56a8509b6af2b05df6d345bd1d35ff08c
Fabrizio Romano <gianchub@gmail.com>
2015-11-04 14:28:15 +0000

    PLAT-245 -- Final version of tests, before factoring helpers out.

--- alembicverify/testing/test_example.py
+++ alembicverify/testing/test_example.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
 from uuid import uuid4
+import json
+import hashlib
 
 from alembic import command
 from alembic.config import Config
@@ -12,6 +14,7 @@ import pytest
 
 from .models import Base
 from alembicverify.comparer import compare
+from test import assert_items_equal
 
 
 db_uri_base = "mysql+mysqlconnector://root:@localhost/alembicverify"
@@ -89,10 +92,10 @@ def _safe_destroy_database(uri):
 
 # # Helpers
 
-def prepare_schema_from_migrations(uri, config):
+def prepare_schema_from_migrations(uri, config, step="head"):
     engine = create_engine(uri)
     script = ScriptDirectory.from_config(config)
-    command.upgrade(config, 'head')
+    command.upgrade(config, step)
     return engine, script
 
 
@@ -156,9 +159,176 @@ def test_same_schema_is_the_same(
     result = compare(uri_left, uri_right, set(['alembic_version']))
 
     # uncomment to see the dump of info dict
-    result.dump_info()
+    # result.dump_info()
 
     assert True == result.is_match
+
+
+@pytest.mark.usefixtures("new_db_left")
+@pytest.mark.usefixtures("new_db_right")
+def test_model_and_migration_schemas_are_not_the_same(
+        uri_left, uri_right, alembic_config_left):
+    """Compare two databases.
+
+    Compares the database obtained with the first migration against the
+    one we get out of the models.  It produces a text file with the
+    results to help debug differences.
+    """
+    prepare_schema_from_migrations(uri_left, alembic_config_left, step="+1")
+    prepare_schema_from_models(uri_right)
+
+    result = compare(uri_left, uri_right, set(['alembic_version']))
+
+    # uncomment to see the dump of errors dict
+    # result.dump_errors()
+
+    errors = {
+        'tables': {
+            'left_only': ['addresses'],
+            'right_only': ['roles']
+        },
+        'tables_data': {
+            'employees': {
+                'columns': {
+                    'left_only': [
+                        {
+                            'default': None,
+                            'name': 'favourite_meal',
+                            'nullable': False,
+                            'type': "ENUM('meat','vegan','vegetarian')"
+                        }
+                    ],
+                    'right_only': [
+                        {
+                            'autoincrement': False,
+                            'default': None,
+                            'name': 'role_id',
+                            'nullable': False,
+                            'type': 'INTEGER(11)'
+                        },
+                        {
+                            'autoincrement': False,
+                            'default': None,
+                            'name': 'number_of_pets',
+                            'nullable': False,
+                            'type': 'INTEGER(11)'
+                        },
+                    ]
+                },
+                'foreign_keys': {
+                    'right_only': [
+                        {
+                            'constrained_columns': ['role_id'],
+                            'name': 'fk_employees_roles',
+                            'options': {},
+                            'referred_columns': ['id'],
+                            'referred_schema': None,
+                            'referred_table': 'roles'
+                        }
+                    ]
+                },
+                'indexes': {
+                    'left_only': [
+                        {
+                            'column_names': ['name'],
+                            'name': 'name',
+                            'type': 'UNIQUE',
+                            'unique': True
+                        }
+                    ],
+                    'right_only': [
+                        {
+                            'column_names': ['role_id'],
+                            'name': 'fk_employees_roles',
+                            'unique': False
+                        },
+                        {
+                            'column_names': ['name'],
+                            'name': 'ix_employees_name',
+                            'type': 'UNIQUE',
+                            'unique': True
+                        }
+                    ]
+                }
+            },
+            'phone_numbers': {
+                'columns': {
+                    'diff': [
+                        {
+                            'key': 'number',
+                            'left': {
+                                'default': None,
+                                'name': 'number',
+                                'nullable': True,
+                                'type': 'VARCHAR(40)'
+                            },
+                            'right': {
+                                'default': None,
+                                'name': 'number',
+                                'nullable': False,
+                                'type': 'VARCHAR(40)'
+                            }
+                        }
+                    ]
+                }
+            }
+        },
+        'uris': {
+            'left': uri_left,
+            'right': uri_right,
+        }
+    }
+
+    compare_error_dicts(errors, result.errors)
+
+
+def compare_error_dicts(err1, err2):
+    assert err1['tables'] == err2['tables']
+    assert err1['uris'] == err2['uris']
+
+    # We cannot directly compare a nested dict structure that has lists
+    # as values. The order of the same list in the two dicts could be
+    # different which would lead to a failure in the comparison, but it
+    # would be wrong, as for us the order doesn't matter and we need
+    # a comparison that only check that the same items are in the lists.
+    # In order to do this, we use the walk function to perform a smart
+    # comparison only on the lists.
+    paths = [
+        ['tables_data', 'employees', 'columns', 'left_only'],
+        ['tables_data', 'employees', 'columns', 'right_only'],
+        ['tables_data', 'employees', 'indexes', 'left_only'],
+        ['tables_data', 'employees', 'indexes', 'right_only'],
+        ['tables_data', 'employees', 'foreign_keys', 'right_only'],
+
+        ['tables_data', 'phone_numbers', 'columns', 'diff'],
+    ]
+
+    for path in paths:
+        assert_items_equal(walk(err1, path), walk(err2, path))
+
+    # one last sanity check:
+    assert sorted(json.dumps(err1)) == sorted(json.dumps(err2))
+
+
+def walk(d, path):
+    """Walks a dict given a path of keys.
+
+    For example, if we have a dict like this::
+
+    d = {
+        'a': {
+            'B': {
+                1: ['hello', 'world'],
+                2: ['hello', 'again'],
+            }
+        }
+    }
+
+    Then `walk(d, ['a', 'B', 1])` would return ['hello', 'world'].
+    """
+    if not path:
+        return d
+    return walk(d[path[0]], path[1:])
 
 
 @pytest.mark.usefixtures("new_db_left")
@@ -167,9 +337,9 @@ def test_model_and_migration_schemas_are_the_same(
         uri_left, uri_right, alembic_config_left):
     """Compare two databases.
 
-    Compares the database obtained with the migrations against the one
-    we get out of the models.  It produces a text file with the results
-    to help debug differences.
+    Compares the database obtained with all migrations against the
+    one we get out of the models.  It produces a text file with the
+    results to help debug differences.
     """
     prepare_schema_from_migrations(uri_left, alembic_config_left)
     prepare_schema_from_models(uri_right)
@@ -177,6 +347,6 @@ def test_model_and_migration_schemas_are_the_same(
     result = compare(uri_left, uri_right, set(['alembic_version']))
 
     # uncomment to see the dump of errors dict
-    result.dump_errors()
+    # result.dump_errors()
 
     assert True == result.is_match
