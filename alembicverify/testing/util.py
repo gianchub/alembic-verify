762599ffbe340e0dccd0bc1f476cf86182ef801c
Fabrizio Romano <gianchub@gmail.com>
2015-11-04 16:04:08 +0000

    PLAT-245 -- Add documentation on utils.

--- alembicverify/testing/util.py
+++ alembicverify/testing/util.py
@@ -14,48 +14,69 @@ from test import assert_items_equal
 
 
 def new_db(uri):
-    """Drop the database and create a brand new one. """
+    """Drop the database at `uri` and create a brand new one. """
     safe_destroy_database(uri)
     create_database(uri)
 
 
 def safe_destroy_database(uri):
-    """Safely destroy a database. """
+    """Safely destroy the database at `uri`. """
     if database_exists(uri):
         drop_database(uri)
 
 
 def get_temporary_uri(uri):
-    """From a base URI, create a temporary one. """
+    """Create a temporary URI given another one.
+
+    For example, given this uri:
+    "mysql+mysqlconnector://root:@localhost/alembicverify"
+
+    a call to `get_temporary_uri``could return something like this:
+    "mysql+mysqlconnector://root:@localhost/test_db_000da...898fe"
+
+    where the last part of the name is taken from a unique ID in hex
+    format.
+    """
     base, _ = uri.rsplit('/', 1)
     uri = '{}/test_db_{}'.format(base, uuid4().hex)
     return uri
 
 
 def make_alembic_config(uri, folder):
+    """Create a configured :class:`alembic.config.Config` object. """
     config = Config()
     config.set_main_option("script_location", folder)
     config.set_main_option("sqlalchemy.url", uri)
     return config
 
 
-def prepare_schema_from_migrations(uri, config, step="head"):
+def prepare_schema_from_migrations(uri, config, revision="head"):
+    """Applies migrations to a database.
+
+    :param string uri: The URI for the database.
+    :param config: A :class:`alembic.config.Config` instance.
+    :param revision: The revision we want to feed to the
+        `command.upgrade` call. Normally it's either "head" or "+1".
+    """
     engine = create_engine(uri)
     script = ScriptDirectory.from_config(config)
-    command.upgrade(config, step)
+    command.upgrade(config, revision)
     return engine, script
 
 
 def prepare_schema_from_models(uri):
+    """Creates the database schema from the `SQLAlchemy` models. """
     engine = create_engine(uri)
     Base.metadata.create_all(engine)
 
 
 def get_current_revision(config, engine, script):
+    """Inspection helper. Get the current revision of a set of migrations. """
     return _get_revision(config, engine, script)
 
 
 def get_head_revision(config, engine, script):
+    """Inspection helper. Get the head revision of a set of migrations. """
     return _get_revision(config, engine, script, revision_type='head')
 
 
@@ -72,7 +93,7 @@ def _get_revision(config, engine, script, revision_type='current'):
 
 
 def compare_error_dicts(err1, err2):
-    """Smart comparison of error dicts.
+    """Smart comparer of error dicts.
 
     We cannot directly compare a nested dict structure that has lists
     as values on some level. The order of the same list in the two dicts
@@ -119,7 +140,7 @@ def walk_dict(d, path):
         }
     }
 
-    Then `walk_dict(d, ['a', 'B', 1])` would return ['hello', 'world'].
+    Then `walk_dict(d, ['a', 'B', 1])` would return `['hello', 'world']`.
     """
     if not path:
         return d
