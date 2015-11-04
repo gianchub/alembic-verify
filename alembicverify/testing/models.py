83c14b73fca102df43070a676e6abb118e5349f9
Fabrizio Romano <gianchub@gmail.com>
2015-11-04 14:27:14 +0000

    PLAT-245 -- Amend models and migrations (keep models for both).

--- alembicverify/testing/models.py
+++ alembicverify/testing/models.py
@@ -25,22 +25,85 @@ class Base(object):
 Base = declarative_base(cls=Base)
 
 
+"""First version of the models - Use these to create the initial migration. """
+
+# class Employee(Base):
+#     __tablename__ = "employees"
+
+#     id = Column(Integer, primary_key=True)
+#     name = Column(Unicode(200), unique=True)
+#     age = Column(Integer, nullable=False, default=18)
+#     ssn = Column(Unicode(30), nullable=False)
+#     favourite_meal = Column(
+#         Enum("meat", "vegan", "vegetarian"),
+#         nullable=False,
+#         default="vegetarian"
+#     )
+
+#     company_id = Column(
+#         Integer,
+#         ForeignKey("companies.id", name="fk_employees_companies"),
+#         nullable=False
+#     )
+
+
+# class Company(Base):
+#     __tablename__ = "companies"
+
+#     id = Column(Integer, primary_key=True)
+#     name = Column(Unicode(200), nullable=False, unique=True)
+
+
+# class PhoneNumber(Base):
+#     __tablename__ = "phone_numbers"
+
+#     id = Column(Integer, primary_key=True)
+#     number = Column(String(40), nullable=True)
+
+#     owner = Column(
+#         Integer,
+#         ForeignKey("employees.id", name="fk_phone_numbers_employees"),
+#         nullable=False
+#     )
+
+
+# class Address(Base):
+#     __tablename__ = "addresses"
+
+#     id = Column(Integer, primary_key=True)
+#     address = Column(Unicode(200), nullable=True)
+#     zip_code = Column(Unicode(20), nullable=True)
+#     city = Column(Unicode(100), nullable=True)
+#     country = Column(Unicode(3), nullable=True)
+
+#     person_id = Column(
+#         Integer,
+#         ForeignKey("employees.id", name="fk_addresses_employees"),
+#         nullable=False
+#     )
+
+
+
+"""Second version of the models - Use these to create the second migration. """
+
 class Employee(Base):
     __tablename__ = "employees"
 
     id = Column(Integer, primary_key=True)
     name = Column(Unicode(200), unique=True, index=True)
-    age = Column(Integer, nullable=False, default=18)
+    age = Column(Integer, nullable=False, default=21)
     ssn = Column(Unicode(30), nullable=False)
-    favourite_meal = Column(
-        Enum("meat", "vegetarian"),
-        nullable=False,
-        default="vegetarian"
-    )
+    number_of_pets = Column(Integer, default=1, nullable=False)
 
     company_id = Column(
         Integer,
-        ForeignKey("companies.id"),
+        ForeignKey("companies.id", name="fk_employees_companies"),
+        nullable=False
+    )
+
+    role_id = Column(
+        Integer,
+        ForeignKey("roles.id", name="fk_employees_roles"),
         nullable=False
     )
 
@@ -60,22 +123,13 @@ class PhoneNumber(Base):
 
     owner = Column(
         Integer,
-        ForeignKey("employees.id"),
+        ForeignKey("employees.id", name="fk_phone_numbers_employees"),
         nullable=False
     )
 
 
-class Address(Base):
-    __tablename__ = "addresses"
+class Role(Base):
+    __tablename__ = "roles"
 
     id = Column(Integer, primary_key=True)
-    address = Column(Unicode(200), nullable=True)
-    zip_code = Column(Unicode(20), nullable=True)
-    city = Column(Unicode(100), nullable=True)
-    country = Column(Unicode(3), nullable=True)
-
-    person_id = Column(
-        Integer,
-        ForeignKey("employees.id"),
-        nullable=False
-    )
+    name = Column(Unicode(50), nullable=False)
